e9c2cc9d3e29f20a1274b9688132f40b8f0c3661
Siraj135 <shaiksirajuddin135@gmail.com>
2023-04-07 18:33:36 +0530

    search

--- client/src/pages/Content/Masajid.js
+++ client/src/pages/Content/Masajid.js
@@ -5,7 +5,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMapLocationDot } from '@fortawesome/free-solid-svg-icons';
 
 const Masajid = () => {
-    // const location = <FontAwesomeIcon icon={faMapLocationDot} />
+    const location = <FontAwesomeIcon icon={faMapLocationDot} />
     // const [masjidData, setMasjidData] = useState([]);
     // const [searchKeyword, setSearchKeyword] = useState('');
     // const [searchData, setSearchData] = useState([]);
@@ -76,47 +76,47 @@ const Masajid = () => {
     //     }
     // }
 
-    // const UserCard = ({ masjid, events, color }) => (
-    //     <Card style={{ width: 'auto' }}>
-    //         <Card.Body>
-    //             <Card.Title>
-    //                 <h4 style={{ display: 'flex', justifyContent: 'space-between' }}>
-    //                     {masjid.masjid_name}
-    //                     <a href={masjid.location} target={'_blank'} rel={"noreferrer"}>{location}</a>
-    //                     {/* <p>{masjid.masjid_name}</p> */}
-    //                 </h4>
-    //             </Card.Title>
-    //             <Table striped bordered hover>
-    //                 <thead>
-    //                     <tr>
-    //                         <th>Namaz</th>
-    //                         <th>Azaan</th>
-    //                         <th>Jamaat</th>
-    //                     </tr>
-    //                 </thead>
-    //                 <tbody>
-    //                     {events.map((event) => (
-    //                         <tr key={event.prayer_id}>
-    //                             <th>{event.prayer_name}</th>
-    //                             <td>
-    //                                 {event.azaan_time.split(":")[0]}:{event.azaan_time.split(":")[1]}
-    //                             </td>
-    //                             <td>
-    //                                 {event.jamaat_time.split(":")[0]}:{event.jamaat_time.split(":")[1]}
-    //                             </td>
-    //                         </tr>
-    //                     ))}
-    //                 </tbody>
-    //                 <tfoot>
-    //                     <tr>
-    //                         <th>Last Updated</th>
-    //                         <th colSpan={2} style={{ color: color }}>{masjid.updated_date}</th>
-    //                     </tr>
-    //                 </tfoot>
-    //             </Table>
-    //         </Card.Body>
-    //     </Card>
-    // );
+    const UserCard = ({ masjid, events, color }) => (
+        <Card style={{ width: 'auto' }}>
+            <Card.Body>
+                <Card.Title>
+                    <h4 style={{ display: 'flex', justifyContent: 'space-between' }}>
+                        {masjid.masjid_name}
+                        <a href={masjid.location} target={'_blank'} rel={"noreferrer"}>{location}</a>
+                        {/* <p>{masjid.masjid_name}</p> */}
+                    </h4>
+                </Card.Title>
+                <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                            <th>Namaz</th>
+                            <th>Azaan</th>
+                            <th>Jamaat</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {events.map((event) => (
+                            <tr key={event.prayer_id}>
+                                <th>{event.prayer_name}</th>
+                                <td>
+                                    {event.azaan_time.split(":")[0]}:{event.azaan_time.split(":")[1]}
+                                </td>
+                                <td>
+                                    {event.jamaat_time.split(":")[0]}:{event.jamaat_time.split(":")[1]}
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                    <tfoot>
+                        <tr>
+                            <th>Last Updated</th>
+                            <th colSpan={2} style={{ color: color }}>{masjid.updated_date}</th>
+                        </tr>
+                    </tfoot>
+                </Table>
+            </Card.Body>
+        </Card>
+    );
 
     // return (
     //     <>
@@ -173,44 +173,72 @@ const Masajid = () => {
 
         fetchCountries();
 
-        const fetchStates = async () => {
-            const country = { queryType: "country", query: selectedCountry, keyword: "state" };
+        // const fetchStates = async () => {
+        //     const country = { queryType: "country", query: selectedCountry, keyword: "state" };
 
-            await axios.post(`http://localhost:4000/api/searchOptions`, country)
-                .then(response => {
-                    setStates(response.data.options);
-                }).catch((err) => {
-                    console.error(err);
-                })
-        }
+        //     await axios.post(`http://localhost:4000/api/searchOptions`, country)
+        //         .then(response => {
+        //             setStates(response.data.options);
+        //         }).catch((err) => {
+        //             console.error(err);
+        //         })
+        // }
 
         const fetchOptions = async (parameters) => {
-            await axios.post(`http://localhost:4000/api/searchOptions`, parameters)
-                .then(response => {
-                    setCities(response.data.options);
-                }).catch((err) => {
-                    console.error(err);
-                })
+            const response = await axios.post(`http://localhost:4000/api/searchOptions`, parameters)
+            const data = await response.data.options;
+
+            return data;
         }
 
-        if (selectedCountry && selectedState && selectedCity) {
-            parameters = { queryType: "state", query: selectedState, keyword: "area" };
-            fetchOptions(parameters);
+        const fetchMasajid = async (parameters) => {
+            const response = await axios.post(`http://localhost:4000/api/masjidByOptions`, parameters)
+            const dataOfMasjid = response.data.data;
+            const timings = response.data.events;
+
+            // setMasjids(dataOfMasjid);
+
+            const totalData = dataOfMasjid.map(masjid => {
+                const current_date = new Date();
+                const updated_date = new Date(masjid.updated_date);
+                const color = Math.round((current_date - updated_date) / (1000 * 60 * 60 * 24)) < 2 ? 'green' : 'red';
+
+                return {
+                    masjid: masjid,
+                    ...timings.find(event => event.timings[0].masjid_id === masjid.masjid_id),
+                    color: color
+                }
+            });
+
+            return totalData;
+        }
+
+        if (selectedCountry && selectedState && selectedCity && selectedArea) {
+            parameters = { queryType: "area", query: selectedArea, keyword: "masjid" };
+            fetchMasajid(parameters)
+                .then(result => setMasjids(result))
+                .catch(error => console.error(error))
+        } else if (selectedCountry && selectedState && selectedCity) {
+            parameters = { queryType: "city", query: selectedCity, keyword: "area" };
+            fetchOptions(parameters)
+                .then(result => setAreas(result))
+                .catch(error => console.error(error))
         } else if (selectedCountry && selectedState) {
             parameters = { queryType: "state", query: selectedState, keyword: "city" };
-            fetchOptions(parameters);
+            fetchOptions(parameters)
+                .then(result => setCities(result))
+                .catch(error => console.error(error))
         } else if (selectedCountry) {
             parameters = { queryType: "country", query: selectedCountry, keyword: "state" };
-            fetchStates(parameters);
+            fetchOptions(parameters)
+                .then(result => setStates(result))
+                .catch(error => console.error(error))
         } else {
             setStates([]);
             setCities([]);
             setAreas([]);
             setMasjids([]);
         }
-
-
-
 
     }, [selectedCountry, selectedState, selectedCity, selectedArea, selectedMasjid]);
 
@@ -241,20 +269,20 @@ const Masajid = () => {
     //         });
     // }, [selectedOption]);
 
-    useEffect(() => {
-        if (searchTerm !== '') {
-            axios
-                .get(`https://api.example.com/search?q=${searchTerm}`)
-                .then((response) => {
-                    setSuggestions(response.data);
-                })
-                .catch((error) => {
-                    console.log(error);
-                });
-        } else {
-            setSuggestions([]);
-        }
-    }, [searchTerm]);
+    // useEffect(() => {
+    //     if (searchTerm !== '') {
+    //         axios
+    //             .get(`https://api.example.com/search?q=${searchTerm}`)
+    //             .then((response) => {
+    //                 setSuggestions(response.data);
+    //             })
+    //             .catch((error) => {
+    //                 console.log(error);
+    //             });
+    //     } else {
+    //         setSuggestions([]);
+    //     }
+    // }, [searchTerm]);
 
     const fetchMasjid = async (searchTerm) => {
         try {
@@ -291,45 +319,64 @@ const Masajid = () => {
     const handleOptionChange = (event) => {
         setSelectedOption(event.target.value);
         setSelectedCountry('');
+        setCountries([]);
         setSelectedState('');
+        setStates([]);
         setSelectedCity('');
+        setCities([]);
         setSelectedArea('');
+        setAreas([]);
         setSelectedMasjid('');
+        setMasjids([]);
     };
 
     const handleCountryChange = (event) => {
         setSelectedCountry(event.target.value);
         setSelectedState('');
+        setStates([]);
         setSelectedCity('');
+        setCities([]);
         setSelectedArea('');
+        setAreas([]);
         setSelectedMasjid('');
+        setMasjids([]);
     };
 
     const handleStateChange = (event) => {
         setSelectedState(event.target.value);
         setSelectedCity('');
+        setCities([]);
         setSelectedArea('');
+        setAreas([]);
         setSelectedMasjid('');
+        setMasjids([]);
     };
 
     const handleCityChange = (event) => {
         setSelectedCity(event.target.value);
         setSelectedArea('');
+        setAreas([]);
         setSelectedMasjid('');
+        setMasjids([]);
     };
 
     const handleAreaChange = (event) => {
         setSelectedArea(event.target.value);
         setSelectedMasjid('');
+        setMasjids([]);
     };
 
     const handleMasjidChange = (event) => {
         setSelectedMasjid(event.target.value);
     };
 
-    const handleSearchChange = (event) => {
-        setSearchTerm(event.target.value);
+    const handleBlur = (event) => {
+        event.target.blur();
     };
+
+    // const handleSearchChange = (event) => {
+    //     setSearchTerm(event.target.value);
+    // };
 
     return (
         <div style={{ display: 'flex', flexDirection: 'column' }}>
@@ -346,7 +393,7 @@ const Masajid = () => {
                 <>
                     <label>
                         Select a country:
-                        <select value={selectedCountry} onChange={handleCountryChange}>
+                        <select value={selectedCountry} onChange={handleCountryChange} style={{ outline: 'none' }} onBlur={handleBlur}>
                             <option value="">-- Select a country --</option>
                             {countries.map((country, index) => (
                                 <option key={index} value={country.id}>
@@ -357,7 +404,7 @@ const Masajid = () => {
                     </label>
                     <label>
                         Select a state:
-                        <select value={selectedState} onChange={handleStateChange}>
+                        <select value={selectedState} onChange={handleStateChange} style={{ outline: 'none' }} onBlur={handleBlur}>
                             <option value="">-- Select a state --</option>
                             {states.map((state, index) => (
                                 <option key={index} value={state.id}>
@@ -368,7 +415,7 @@ const Masajid = () => {
                     </label>
                     <label>
                         Select a city:
-                        <select value={selectedCity} onChange={handleCityChange}>
+                        <select value={selectedCity} onChange={handleCityChange} style={{ outline: 'none' }} onBlur={handleBlur}>
                             <option value="">-- Select a city --</option>
                             {cities.map((city, index) => (
                                 <option key={index} value={city.id}>
@@ -379,7 +426,7 @@ const Masajid = () => {
                     </label>
                     <label>
                         Select a area:
-                        <select value={selectedArea} onChange={handleAreaChange}>
+                        <select value={selectedArea} onChange={handleAreaChange} style={{ outline: 'none' }} onBlur={handleBlur}>
                             <option value="">-- Select a area --</option>
                             {areas.map((area, index) => (
                                 <option key={index} value={area.id}>
@@ -392,7 +439,7 @@ const Masajid = () => {
                 {selectedOption === 'masjid' && (
                     <label>
                         Select a masjid name:
-                        <select value={selectedMasjid} onChange={handleMasjidChange}>
+                        <select value={selectedMasjid} onChange={handleMasjidChange} style={{ outline: 'none' }} onBlur={handleBlur}>
                             <option value="">-- Select a masjid --</option>
                             {masjids.map((masjid, index) => (
                                 <option key={index} value={masjid.id}>
@@ -403,12 +450,12 @@ const Masajid = () => {
                     </label>
                 )}
             </div>
-            <div style={{ display: 'flex', justifyContent: 'center' }}>
+            {/* <div style={{ display: 'flex', justifyContent: 'center' }}>
                 <label>
                     Search:
                     <input type="text" value={searchTerm} onChange={handleSearchChange} />
                 </label>
-            </div>
+            </div> */}
             <div style={{ display: 'flex', justifyContent: 'center' }}>
                 {suggestions.length > 0 && (
                     <ul>
@@ -418,6 +465,18 @@ const Masajid = () => {
                     </ul>
                 )}
             </div>
+            {
+                masjids ? (
+                    <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
+                        {masjids.map((masjid) => (
+                            <div style={{ margin: '10px' }} key={masjid.masjid.masjid_id}>
+                                <UserCard masjid={masjid.masjid} events={masjid.timings} color={masjid.color} />
+                            </div>
+                        ))}
+                    </div>) : (<div style={{ margin: '10px', padding: '100px', fontSize: '50px' }} key="user.id">
+                        <p style={{ textAlign: 'center', cursor: 'pointer' }}>No masjid added yet.</p>
+                    </div>)
+            }
         </div>
     )
 }
